9a7027a7777742e5ccdc656a44d5bb445aeb1f46
MartinBurga <mburga.xixv@gmail.com>
2025-06-16 08:38:34 -0500

    Creacion de modelo Notes con estructura basada en MVVM: Adicionalmente, creacion de la carpeta ViewModels y sus elementos

---
+++ TallerClaseMVVMAppGrupo4/ViewModels/NoteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerClaseMVVMAppGrupo4.ViewModels
+{
+    class NoteViewModel
+    {
+    }
+}

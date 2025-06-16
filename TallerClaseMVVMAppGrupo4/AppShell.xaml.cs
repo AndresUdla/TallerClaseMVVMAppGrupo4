60ebd93d85fcbaf21732d7a3595bce2eea41ab06
Josue Mullo <josue.mullo@udla.edu.ec>
2025-06-16 07:44:38 -0500

    Add project files.

---
+++ TallerClaseMVVMAppGrupo4/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TallerClaseMVVMAppGrupo4
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
